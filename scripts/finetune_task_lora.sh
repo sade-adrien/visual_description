e09fe2a0c9c3fe8845d437089c4a4340a8ce04cd
Adrien <sade-adrien@live.fr>
2024-07-15 14:14:43 -0500

    llava ft

---
+++ scripts/finetune_task_lora.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+deepspeed ../LLaVA/llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed ../LLaVA/scripts/zero3.json \
+    --model_name_or_path liuhaotian/llava-v1.6-vicuna-7b \
+    --version v1 \
+    --data_path ../data/train2014_llava.json \
+    --eval_data_path ../data/val2014_llava.json \
+    --image_folder ../data/ \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ../models/llava-v1.6-vicuna-7b-VD \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --max_steps 20000 \
+    --evaluation_strategy "steps" \
+    --eval_steps 1000 \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 20 \
+    --learning_rate 5e-5 \
+    --weight_decay 0.05 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --model_max_length 2048 \
+    --gradient_checkpointing False \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
